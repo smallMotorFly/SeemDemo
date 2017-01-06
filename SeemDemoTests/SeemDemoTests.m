745f122c5d14795b2f4f6b791f5e4fefb93bd0ca
zhangbin <zhangbin@handlecar.com>
2017-01-06 23:11:34 +0800

    Initial Commit

---
+++ SeemDemoTests/SeemDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  SeemDemoTests.m
+//  SeemDemoTests
+//
+//  Created by Handlecar1 on 17/1/6.
+//  Copyright © 2017年 Handlecar1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SeemDemoTests : XCTestCase
+
+@end
+
+@implementation SeemDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
